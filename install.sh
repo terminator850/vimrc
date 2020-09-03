71ca16a3209c35c050418ae9743a7c77f224bba1
wangyu <wangyu2017@qq.com>
2020-09-03 08:39:56 +0800

    add install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,28 @@
-sudo pacman -S vim 
+mkdir ($HOME)/workspace
+echo "====install git===="
 sudo pacman -S git
-sudo pacman -S go
-sudo pacman -S vscode
-sudo pacman -S make cmake
-sudo pacman -S powerline powerline-vim
+echo "====git ok===="
+echo "====install vim===="
+sudo pacman -S vim 
+# git clone 
+# cp asdfafa ($HOME)/.vimrc
+echo "!!!!youcompleteme需要手动安装"
+echo "====vim ok===="
+echo "====install golang===="
+# mkdir -p ($HOME)/workspace/gowork
+# sudo pacman -S go
+# go env -w GO111MODULE=on
+# go env -w GOPROXY=https://goproxy.cn,direct
+# go env -w GOPATH=($HOME)/workspace/gowork
+# go install golang.org/x/tools/cmd/gopls
+echo "====golang ok===="
+echo "====install tools===="
+# sudo pacman -S vscode
+# sudo pacman -S make cmake
+echo "====tools ok===="
+echo "====install the other===="
+# sudo pacman -S powerline powerline-vim
+# sudo pacman -S qtorrent
+# sudo pacman -S freemind
+# sudo pacman -S cozy-desktop
+echo "====the other ok===="
